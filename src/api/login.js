85a692952383a27772c20280be7f03fe94962cb8
Ophir Bushinsky <ophirbushi@gmail.com>
2019-05-17 20:03:07 +0300

    add docker

--- src/api/login.js
+++ src/api/login.js
@@ -10,10 +10,7 @@ module.exports = async (req, res) => {
     try {
         const user = await User.findOne({ username });
         if (!user || !user.salt || !user.hash) return res.sendStatus(401);
-
-        const { salt, hash } = user;
-        if (hash !== hashPassword(password, salt)) return res.sendStatus(401);
-
+        if (user.hash !== hashPassword(password, user.salt)) return res.sendStatus(401);
         const token = sign({ username });
         return res.send(token);
     } catch (err) {
--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ const crypto = require('crypto');
 
 module.exports = {
     port: 8080,
-    DB_URI: 'mongodb://localhost:27017/identity-service',
+    DB_URI: process.env.DB_URI,
     jwtExpiresIn: '5h',
     jwtSecret: process.env.jwtSecret || crypto.randomBytes(32).toString('hex')
 };
